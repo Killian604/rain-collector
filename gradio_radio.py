926b6fc73002544bde3f9186319fe8d0df565a1c
Killian Stacey <killianstacey5@gmail.com>
2025-01-01 10:45:13 -0800

    gradio_radio: add 2 formalized functions for TTS inference

--- gradio_radio.py
+++ gradio_radio.py
@@ -4,29 +4,40 @@ For running radio app
 Usage: `gradio <ThisFileName>`
 """
 from typing import Dict, List, Optional, Tuple
-from backend import gradio_logic, logging_extra as le, shared, vllm_util
+from backend import gradio_logic, logging_extra as log, shared, vllm_util
 from parler_tts import ParlerTTSForConditionalGeneration
 from transformers import AutoTokenizer
-import chromadb
+# import chromadb
 import gradio as gr
 import os
 import random
+import re
 import soundfile as sf
-import torch
 import time
+import torch
+# from TTS.api import TTS
 
 # Magic variables
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # Define the vLLM server URL
 VLLM_SERVER_IP, VLLM_SERVER_PORT = 'localhost', 8000
 VLLM_SERVER_IP, VLLM_SERVER_PORT = '10.0.0.73', 8000
+voice_clone_wav_fp = '/home/killfm/Videos/aj/aj_cryptic_andjustthatlittlebit.wav'
+reference_voice_text = "And just that little bit makes me wild. And so, they can get them, because they're powerful, they're smart, they're neat"
 DEFAULT_TEMP = 1.0
 MAX_TOKENS = 512
 
 if gr.NO_RELOAD:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", ).to(device)
+    # print(f'\n{TTS().list_models()=}\n')
     model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-mini-v1").to(device)
     tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-mini-v1")
     torch.compile()
+
+
+assert os.path.isfile(voice_clone_wav_fp)
+
 
 # Save shared
 shared.VLLM_SERVER_URL = vllm_util.create_vllm_chat_uri(VLLM_SERVER_IP, VLLM_SERVER_PORT)  # f"http://{VLLM_SERVER_IP}:{VLLM_SERVER_PORT}/v1/chat/completions"  # Update with your actual server URL
@@ -69,11 +80,59 @@ def request_llm_response_to_chat_history(chatbot_history_to_send_to_llm: List[di
 
 
 @torch.inference_mode()
-def click_generate_tape(prompt: Optional[str] = None, outfilepath: Optional[str] = None, debug: bool = True):
+def infer_to_file_parler(tts_text: str, output_fp: str, description_of_speaker: str, **kwargs):
+    # PARLER SECTION
+    # pad_token_id = model.config.pad_token_id
+    description_ids = tokenizer(description_of_speaker, return_tensors="pt").input_ids.to(device)
+    prompt_ids = tokenizer(tts_text, return_tensors="pt").input_ids.to(device)
+    generation_kwargs = {
+        "attention_mask": torch.ones_like(description_ids),
+        'temperature': 0.7,
+
+    }
+    with log.Timer('Logging Parler response generation timing'):
+        generation = model.generate(
+            input_ids=description_ids,
+            prompt_input_ids=prompt_ids,
+            **generation_kwargs
+        )
+    audio_arr = generation.cpu().numpy().squeeze()
+    sf.write(output_fp, audio_arr, model.config.sampling_rate)
+
+@torch.inference_mode()
+def infer_to_file_coqui(tts_text: str, output_fp, reference_audio_fp: Optional[str] = None, **kwargs):
+    # COQUI SECTION
+
+    # Run TTS
+    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+    # Text to speech list of amplitude values as output
+    # wav = tts.tts(
+    #     text=reference_voice_text,
+    #     language="en",
+    #     speaker_wav=voiceclonefile,
+    #     emotion=None,
+    #     speed=1.0,
+    # )
+    # # print(f'{wav=} // {type(wav)=}')
+    print(f'{tts_text=}')
+    # Text to speech to a file
+    _output_fp: str = tts.tts_to_file(
+        text=tts_text,
+        language="en",
+        speaker_wav=reference_audio_fp,
+        emotion='energetic',
+        speed=1.5,
+        file_path=output_fp,
+        split_sentences=True,
+    )
+    # print(f'tts.tts_to_file returns: {type(_output_fp)=} // {_output_fp=}')
+
+
+def click_generate_tape(prompt: Optional[str] = None, output_fp: Optional[str] = None, debug: bool = True):
     """
 
     :param prompt:
-    :param outfilepath:
+    :param output_fp:
     :return:
     """
     print(f'Fetching recording...')
@@ -81,19 +140,20 @@ def click_generate_tape(prompt: Optional[str] = None, outfilepath: Optional[str]
     prompt: str = random.choice(shared.possible_tape_prompts)  # TODO: Remove me later
     if debug:
         print(f'{prompt=} / {type(prompt)=}')
-    outfilepath = outfilepath or os.path.join(shared.repopath, 'output_audio', time.strftime('%y%m%d-%H%M%S')+'.mp3')
+    output_fp = output_fp or os.path.join(shared.repopath, 'output_audio', time.strftime('%y%m%d-%H%M%S') + '.mp3')
+
     # prompt = "Hey, how are you doing today?"
     convo_history_a = convo_history.copy()
     convo_history_a.append({'role': 'user', 'content': prompt})
     if debug:
         print(f'{convo_history_a=}')
-    with le.Timer('Logging VLLM response time'):
+    with log.Timer('Logging VLLM response time'):
         content, _role = vllm_util.generate_response(convo_history_a, shared.CURRENT_ML_MODEL, shared.VLLM_SERVER_URL, False, max_tokens=MAX_TOKENS, temperature=DEFAULT_TEMP)
     # parler_text = ""
     # for content, _response_role in vllm_util.yield_streaming_response(convo_history_a, shared.CURRENT_ML_MODEL, shared.VLLM_SERVER_URL, max_tokens=MAX_TOKENS, temperature=DEFAULT_TEMP, stream=True):
     #     parler_text += content
-    tts_text = content.strip()
-    print(f'{tts_text=}')
+    tts_text = re.sub(r'\s+', ' ', content.strip())
+    print(f'\n\n{tts_text=}\n\n')
     # prompt = """
     # A delight surrounding the Mathematics of Finance is that while much is known, so
     # much is unknown. Consequently, with the current state of understanding, it is wise
@@ -103,24 +163,11 @@ def click_generate_tape(prompt: Optional[str] = None, outfilepath: Optional[str]
     # description = "Jon's voice is monotone yet slightly fast in delivery, with a very close recording that almost has no background noise."
     description = shared.generate_caller_desc()
     # parler_text = description
-    pad_token_id = model.config.pad_token_id
-    description_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
-    prompt_ids = tokenizer(tts_text, return_tensors="pt").input_ids.to(device)
-    generation_kwargs = {
-        "attention_mask": torch.ones_like(description_ids),
-        'temperature': 0.7,
 
-    }
-    with le.Timer('Logging Parler response generation timing'):
-        generation = model.generate(
-            input_ids=description_ids,
-            prompt_input_ids=prompt_ids,
-            **generation_kwargs
-        )
-    audio_arr = generation.cpu().numpy().squeeze()
-    sf.write(outfilepath, audio_arr, model.config.sampling_rate)
-    print(f'Output written to: {outfilepath}')
-    return outfilepath
+    # infer_to_file_coqui(tts_text, voice_clone_wav_fp, output_fp)
+    infer_to_file_parler(tts_text, output_fp, description)
+    print(f'Output written to: {output_fp}')
+    return output_fp
 
 
 # GRADIO BLOCKS
