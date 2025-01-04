8a1a5d5ac00c5069e58de25277b612e6ffdf3818
Killian Stacey <killianstacey5@gmail.com>
2025-01-04 08:12:01 -0800

    gradio_radio: formalize inf, add framework for modifying generation

--- gradio_radio.py
+++ gradio_radio.py
@@ -5,7 +5,7 @@ Usage: `gradio <ThisFileName>`
 """
 from typing import Dict, List, Optional, Tuple
 from backend import gradio_logic, logging_extra as log, shared, vllm_util
-from parler_tts import ParlerTTSForConditionalGeneration
+# from parler_tts import ParlerTTSForConditionalGeneration
 from transformers import AutoTokenizer
 # import chromadb
 import gradio as gr
@@ -15,25 +15,29 @@ import re
 import soundfile as sf
 import time
 import torch
-# from TTS.api import TTS
+from TTS.api import TTS
 
 # Magic variables
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 # Define the vLLM server URL
 VLLM_SERVER_IP, VLLM_SERVER_PORT = 'localhost', 8000
 VLLM_SERVER_IP, VLLM_SERVER_PORT = '10.0.0.73', 8000
-voice_clone_wav_fp = '/home/killfm/Videos/aj/aj_cryptic_andjustthatlittlebit.wav'
+voice_clone_wav_fp = '/home/killfm/Videos/aj/aj_cryptic_andjustthatlittlebit.wav'# aj_evan
+voice_clone_wav_fp = '/home/killfm/Videos/aj/aj_evangelion.wav'
 reference_voice_text = "And just that little bit makes me wild. And so, they can get them, because they're powerful, they're smart, they're neat"
-DEFAULT_TEMP = 1.0
-MAX_TOKENS = 512
+speech_speed = 1.50
+DEFAULT_TEMP = 0.8
+MAX_TOKENS = 1024
 
 if gr.NO_RELOAD:
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    # tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", ).to(device)
+    device = "cuda"  # if torch.cuda.is_available() else "cpu"
+    model = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+    # model = TTS("tts_models/multilingual/multi-dataset/your_tts").to(device)
+    # model = TTS("tts_models/multilingual/multi-dataset/bark").to(device)  # Limited for voice cloning, but interesting to say the least
     # print(f'\n{TTS().list_models()=}\n')
-    model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-mini-v1").to(device)
-    tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-mini-v1")
-    torch.compile()
+    # model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-mini-v1").to(device)
+    # tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-mini-v1")
+    torch.compile(model)
 
 
 assert os.path.isfile(voice_clone_wav_fp)
@@ -81,49 +85,57 @@ def request_llm_response_to_chat_history(chatbot_history_to_send_to_llm: List[di
 
 @torch.inference_mode()
 def infer_to_file_parler(tts_text: str, output_fp: str, description_of_speaker: str, **kwargs):
-    # PARLER SECTION
     # pad_token_id = model.config.pad_token_id
-    description_ids = tokenizer(description_of_speaker, return_tensors="pt").input_ids.to(device)
-    prompt_ids = tokenizer(tts_text, return_tensors="pt").input_ids.to(device)
-    generation_kwargs = {
-        "attention_mask": torch.ones_like(description_ids),
-        'temperature': 0.7,
+    # description_ids = tokenizer(description_of_speaker, return_tensors="pt").input_ids.to(device)
+    # prompt_ids = tokenizer(tts_text, return_tensors="pt").input_ids.to(device)
+    # generation_kwargs = {"attention_mask": torch.ones_like(description_ids), 'temperature': 0.7, }
+    # with log.Timer('Logging Parler response generation timing'):
+    #     generation = model.generate(
+    #         prompt_input_ids=prompt_ids,
+    #         input_ids=description_ids,
+    #         **generation_kwargs
+    #     )
+    # audio_arr = generation.cpu().numpy().squeeze()
+    # print(f'{audio_arr.max()=} // {audio_arr.dtype=}')
+    # sf.write(output_fp, audio_arr, model.config.sampling_rate)
+    # pad_token_id = model.config.pad_token_id
 
+    input_ids = tokenizer(description_of_speaker, return_tensors="pt").input_ids.to(device)
+    prompt_input_ids = tokenizer(tts_text, return_tensors="pt").input_ids.to(device)
+    generation_kwargs = {
+        "attention_mask": torch.ones_like(input_ids),
+        'temperature': 1.0,
     }
-    with log.Timer('Logging Parler response generation timing'):
-        generation = model.generate(
-            input_ids=description_ids,
-            prompt_input_ids=prompt_ids,
-            **generation_kwargs
-        )
-    audio_arr = generation.cpu().numpy().squeeze()
+
+    generation = model.generate(
+        input_ids=input_ids,
+        prompt_input_ids=prompt_input_ids,
+        **generation_kwargs
+    )
+    audio_arr = generation.detach().cpu().numpy().squeeze()
     sf.write(output_fp, audio_arr, model.config.sampling_rate)
+
 
 @torch.inference_mode()
 def infer_to_file_coqui(tts_text: str, output_fp, reference_audio_fp: Optional[str] = None, **kwargs):
-    # COQUI SECTION
-
-    # Run TTS
-    # ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
-    # Text to speech list of amplitude values as output
-    # wav = tts.tts(
-    #     text=reference_voice_text,
-    #     language="en",
-    #     speaker_wav=voiceclonefile,
-    #     emotion=None,
-    #     speed=1.0,
-    # )
-    # # print(f'{wav=} // {type(wav)=}')
+    # Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+    # tts_text = '[laughter] Is this the bark model? Are you kidding me? 99.5 FM? I cant believe my luck!!!!'
     print(f'{tts_text=}')
     # Text to speech to a file
-    _output_fp: str = tts.tts_to_file(
+    files = [
+        '/home/killfm/Videos/aj/aj_evangelion.wav',
+        '/home/killfm/Videos/dana.mp3',
+    ]
+    reference_wav = random.choice(files)
+    print(f'{reference_wav=}')
+    _output_fp: str = model.tts_to_file(
         text=tts_text,
         language="en",
-        speaker_wav=reference_audio_fp,
-        emotion='energetic',
-        speed=1.5,
+        speaker_wav=reference_wav,
+        speed=speech_speed,
         file_path=output_fp,
         split_sentences=True,
+        # emotion='Neutral',
     )
     # print(f'tts.tts_to_file returns: {type(_output_fp)=} // {_output_fp=}')
 
@@ -141,6 +153,7 @@ def click_generate_tape(prompt: Optional[str] = None, output_fp: Optional[str] =
     if debug:
         print(f'{prompt=} / {type(prompt)=}')
     output_fp = output_fp or os.path.join(shared.repopath, 'output_audio', time.strftime('%y%m%d-%H%M%S') + '.mp3')
+    output_text_fp = output_fp.replace('.mp3', '.txt')
 
     # prompt = "Hey, how are you doing today?"
     convo_history_a = convo_history.copy()
@@ -148,11 +161,10 @@ def click_generate_tape(prompt: Optional[str] = None, output_fp: Optional[str] =
     if debug:
         print(f'{convo_history_a=}')
     with log.Timer('Logging VLLM response time'):
-        content, _role = vllm_util.generate_response(convo_history_a, shared.CURRENT_ML_MODEL, shared.VLLM_SERVER_URL, False, max_tokens=MAX_TOKENS, temperature=DEFAULT_TEMP)
-    # parler_text = ""
-    # for content, _response_role in vllm_util.yield_streaming_response(convo_history_a, shared.CURRENT_ML_MODEL, shared.VLLM_SERVER_URL, max_tokens=MAX_TOKENS, temperature=DEFAULT_TEMP, stream=True):
-    #     parler_text += content
-    tts_text = re.sub(r'\s+', ' ', content.strip())
+        content, _role = vllm_util.generate_response(convo_history_a, shared.CURRENT_ML_MODEL, shared.VLLM_SERVER_URL, stream=False, max_tokens=MAX_TOKENS, temperature=DEFAULT_TEMP)
+    tts_text = re.sub(r'\s+', ' ', content.strip()).replace('\n', ' ')
+    with open(output_text_fp, 'w') as f:
+        f.write(tts_text+'\n')
     print(f'\n\n{tts_text=}\n\n')
     # prompt = """
     # A delight surrounding the Mathematics of Finance is that while much is known, so
@@ -162,11 +174,11 @@ def click_generate_tape(prompt: Optional[str] = None, output_fp: Optional[str] =
     # description = "A young female speaker delivers a slightly expressive and animated speech with a medium slow speed and pitch. The recording sounds like a clip from a radio station."
     # description = "Jon's voice is monotone yet slightly fast in delivery, with a very close recording that almost has no background noise."
     description = shared.generate_caller_desc()
-    # parler_text = description
 
-    # infer_to_file_coqui(tts_text, voice_clone_wav_fp, output_fp)
-    infer_to_file_parler(tts_text, output_fp, description)
-    print(f'Output written to: {output_fp}')
+    infer_to_file_coqui(tts_text, output_fp, voice_clone_wav_fp)
+    # infer_to_file_parler(tts_text, output_fp, description)
+    if debug:
+        print(f'Output written to: {output_fp}')
     return output_fp
 
 
@@ -180,21 +192,39 @@ with gr.Blocks(
 ) as demo:
     # with gr.Tab('Audio'):
     with gr.Row():
-        state_mostrecent_reply = gr.State([None])
-        btn_new_recording = gr.Button('Listen to new voice recording')
+        slider_voicespeed = gr.Slider(
+            label='Voice speed',
+            value=speech_speed,
+            minimum=0.1,
+            maximum=2.0,
+            step=0.1,
+            visible=True,
+            interactive=True,
+        )
         audio_msg = gr.Audio(
             label='Listen to tape',
             sources=['upload'],
             type='filepath',
         )
-        btn_new_recording.click(click_generate_tape, None, audio_msg)
 
+        def update_checkbox(checkbox):
+            print(f'{checkbox=} // {type(checkbox)=}')
+            print(f'before: {slider_voicespeed.visible}')
+            slider_voicespeed.visible = not checkbox
+            slider_voicespeed.interactive = not checkbox
+            slider_voicespeed.render()
+            print(f'after: {slider_voicespeed.visible}')
+
+
+    with gr.Row():
+        btn_new_recording = gr.Button('Listen to new voice recording')
+        btn_new_recording.click(click_generate_tape, None, audio_msg)
     with gr.Row():
         pass
     # with gr.Tab('Chat'):
     with gr.Row():
         with gr.Column():
-            chatbot = gr.Chatbot(convo_history, height=768, type='messages', show_copy_button=True, )
+            chatbot = gr.Chatbot(convo_history, height=512, type='messages', show_copy_button=True, )
             input_textbox_str = gr.Textbox()
             clear_button = gr.Button("Clear")
         with gr.Column():
